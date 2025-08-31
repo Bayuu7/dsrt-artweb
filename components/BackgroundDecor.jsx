2fc567067c7945f271ea15aa9dd6c234f3fc657d
Bayuu7 <103884982+Bayuu7@users.noreply.github.com>
2025-08-31 15:34:23 +0700

    Create BackgroundDecor.jsx

---
+++ components/BackgroundDecor.jsx
@@ -0,0 +1,12 @@
+export default function BackgroundDecor() {
+  return (
+    <>
+      <svg className="blob blob1" viewBox="0 0 600 600">
+        <path fill="#a18fff" d="M600,300Q554,400,450,470Q346,540,220,510Q94,480,80,360Q66,240,140,150Q214,60,340,90Q466,120,573,210Q600,255,600,300Z"/>
+      </svg>
+      <svg className="blob blob2" viewBox="0 0 600 600">
+        <path fill="#ff78a5" d="M600,300Q554,400,450,470Q346,540,220,510Q94,480,80,360Q66,240,140,150Q214,60,340,90Q466,120,573,210Q600,255,600,300Z"/>
+      </svg>
+    </>
+  );
+}
